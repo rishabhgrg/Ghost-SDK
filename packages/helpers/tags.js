4cb12fab7623fc8a03b139c6ce6ec002a87cb0a0
Hannah Wolfe <erisds@gmail.com>
2018-10-08 12:27:41 -0400

    ✨Added working tags helper
    - filters tags according to visibility
    - applies limit, from and to
    - applies fn to each tag if available, else just returns tag.name

--- packages/helpers/tags.js
+++ packages/helpers/tags.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+const visibility = require('./utils/visibility');
 /**
  * Tags Helper
  *
@@ -6,7 +8,26 @@
  */
 
 module.exports = (data, options) => {
-    console.log('testing');
-}
+    let output = '';
+    let separator = _.isString(options.separator) ? options.separator : ', ';
+    let prefix = _.isString(options.prefix) ? options.prefix : '';
+    let suffix = _.isString(options.suffix) ? options.suffix : '';
+    let limit = options.limit ? parseInt(options.limit, 10) : undefined;
+    let from = options.from ? parseInt(options.from, 10) : 1;
+    let to = options.to ? parseInt(options.to, 10) : undefined;
+    let visibilityArr = visibility.parse(options.visibility);
+    let explicit = options.explict || !!options.visibility;
 
-module.exports.ALLOWED_OPTIONS = ['separator'];
+    if (data.tags && data.tags.length) {
+        output = visibility.filter(data.tags, visibilityArr, explicit, options.fn);
+        from -= 1; // From uses 1-indexed, but array uses 0-indexed.
+        to = to || limit + from || output.length;
+        output = output.slice(from, to).join(separator);
+    }
+
+    if (output) {
+        output = prefix + output + suffix;
+    }
+
+    return output;
+}
