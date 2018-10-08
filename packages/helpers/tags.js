90a23ea6a4ad8f2d812ec4aacf8d7f269f9c7c28
Hannah Wolfe <erisds@gmail.com>
2018-10-08 11:32:04 -0400

    ✨ Added  new helper packages - initial commit

---
+++ packages/helpers/tags.js
@@ -0,0 +1,12 @@
+/**
+ * Tags Helper
+ *
+ * @param {object} data - the data we are filtering
+ * @param {object} options - filter options
+ */
+
+module.exports = (data, options) => {
+    console.log('testing');
+}
+
+module.exports.ALLOWED_OPTIONS = ['separator'];
